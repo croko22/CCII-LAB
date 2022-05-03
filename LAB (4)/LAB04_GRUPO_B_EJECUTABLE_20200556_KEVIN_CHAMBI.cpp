8b81517c48d282306ca975aab2d444c6fe91a4f4
Kevin <kchambit@unsa.edu.pe>
2022-05-03 18:30:33 -0500

    Previa lab 4

---
+++ LAB (4)/LAB04_GRUPO_B_EJECUTABLE_20200556_KEVIN_CHAMBI.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+//KEVIN JOAQUIN CHAMBI TAPIA | CUI:20200556
+
+//Ejercicio 1
+
+int main(int argc, char const *argv[])
+{
+	cout<<"Hola";
+	return 0;
+}
+
+////Ejercicio 2
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 3
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 4
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 5
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 6
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 7
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 8
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 9
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
+
+////Ejercicio 10
+//
+//int main(int argc, char const *argv[])
+//{
+//
+//	return 0;
+//}
